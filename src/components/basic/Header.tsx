da455e2fd9cba2de1f309e62922f391e43ea0cb3
pcolta <p.colta1@gmail.com>
2023-02-12 13:11:03 +0100

    fix styles when valid, active and focus

--- src/components/basic/Header.tsx
+++ src/components/basic/Header.tsx
@@ -1,21 +1,25 @@
 import * as React from "react";
 import logo from '@/assets/images/Logo.svg';
 import {BsHeart, BsBasket, BsPerson, BsGear} from "react-icons/bs";
+import useAuth from "../../hooks/useAuth";
 
-const Header = () => (
-    <div className="header">
-        <div className="header__wrapper">
-            <div className="header__logo">
-                <img src={logo}/>
+export default function Header() {
+    const {logout} = useAuth();
+    return (
+        <div className="header">
+            <div className="header__wrapper">
+                <div className="header__logo">
+                    <img src={logo}/>
+                </div>
+                <ul>
+                    <li className="header__link"><BsBasket/></li>
+                    <li className="header__link"><BsHeart/></li>
+                    <li className="header__link"><BsGear/></li>
+                    <li className="header__link"><BsPerson/></li>
+                    <li className="header__link" onClick={logout}>Logout</li>
+                </ul>
             </div>
-            <ul>
-                <li className="header__link"><BsBasket/></li>
-                <li className="header__link"><BsHeart/></li>
-                <li className="header__link"><BsGear/></li>
-                <li className="header__link"><BsPerson/></li>
-            </ul>
         </div>
-    </div>
-)
+    )
 
-export default Header
+}
--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import useAuth from "../../hooks/useAuth";
 
 export default function HomePage() {
-    const { logout } = useAuth();
+
 
     return (
         <div className="home">
-
-            <button type="button" onClick={logout}>
-                Logout
-            </button>
+            {/*HOME*/}
         </div>
     );
 }
